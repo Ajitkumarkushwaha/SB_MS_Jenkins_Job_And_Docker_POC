bfdefbb653699a8c0a60715b6fb1e0b1c9730d03
Ajit Kumar <naa.kumarajit@gmai.com>
2021-03-22 01:38:49 +0530

    Welcome Message is added

--- src/main/java/com/kushwaha/SbMsJenkinsJobAndDockerPocApplication.java
+++ src/main/java/com/kushwaha/SbMsJenkinsJobAndDockerPocApplication.java
@@ -2,10 +2,19 @@ package com.kushwaha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SbMsJenkinsJobAndDockerPocApplication {
 
+	
+	@GetMapping(value = "/welcome")
+	public String printWelcomeMsg() {
+		return "Welcome to Jenkins Job and Docker Integration !!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SbMsJenkinsJobAndDockerPocApplication.class, args);
 	    System.out.println("Application Started !!");
