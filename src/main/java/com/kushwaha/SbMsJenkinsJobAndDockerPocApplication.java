237706cfef3df57b21ab0b208efeff259267fe4e
Ajit Kumar <naa.kumarajit@gmai.com>
2021-03-22 01:22:59 +0530

    Initial Project Created

---
+++ src/main/java/com/kushwaha/SbMsJenkinsJobAndDockerPocApplication.java
@@ -0,0 +1,14 @@
+package com.kushwaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMsJenkinsJobAndDockerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMsJenkinsJobAndDockerPocApplication.class, args);
+	    System.out.println("Application Started !!");
+	}
+
+}
---
+++ src/test/java/com/kushwaha/SbMsJenkinsJobAndDockerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushwaha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMsJenkinsJobAndDockerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
